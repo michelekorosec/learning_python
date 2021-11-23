b69fb3d8bb7b766298ae6e18e148b6f24390909c
Michele Korosec <72503894+michelekorosec@users.noreply.github.com>
2021-11-23 10:31:35 +0100

    Adjusted spacing

--- NumpyTutorial_RealPython/grades.py
+++ NumpyTutorial_RealPython/grades.py
@@ -1,7 +1,11 @@
 import numpy as np
+
 CURVE_CENTER = 80
+
 grades = np.array([72, 35, 64, 88, 51, 90, 74, 12])
+
 def curve(grades):
+
     average = grades.mean()
     change = CURVE_CENTER - average
     new_grades = grades + change
