42c41ced26f38f7f1abb62e1cc5f1845d4dc742d
michelekorosec <72503894+michelekorosec@users.noreply.github.com>
2022-05-11 20:06:03 +0200

    Added Audio_Book child class and Type Error for audio_length

--- Book_DataBase/Book_DataBase.md
+++ Book_DataBase/Book_DataBase.md
@@ -2,17 +2,17 @@
 
 ##Purpose
 
-This Database should serve as a tracking systems for the books I have read throughout the years.
+This Database should serve as a tracking system for the books I have read throughout the years.
 I want to keep track of different book and author characteristics by creating author and book objects with specific attributes.
 The books I have read will then be organized in a Pandas DataFrame and plots on the gathered data shall be created.
-Ideally I will find a way to sync this Python database with an existing Microsoft Excel sheet containing data from the years 2020 - 2022.
+Ideally I will find a way to sync this Python database with an existing Microsoft Excel sheet containing data from the years 2020 - 2022 or my Goodreads account.
 
 ##Plan of Action
 
 - Create an Author Class
     - Attributes: Name, Nationality, Gender
 - Create a Book Class
-    - Attributes: Title, Author (author class!), Type (audio/physical → child classes), Source (Library, bought, etc), Owned (y/n), Date Started, Date Finished, Series (y/n), Genre, Publication Year, Rating, # of times read, Reading Language
+    - Attributes: Title, Author (author class!), Format (audio/physical → child classes), Source (Library, bought, etc), Owned (y/n), Date Started, Date Finished, Series (y/n), Genre, Publication Year, Rating, # of times read, Reading Language
     - Audio Child Class
         - Attributes: Audio length
     - Physical Child Class
--- Book_DataBase/book_database.py
+++ Book_DataBase/book_database.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import datetime
 
 valid_author_genders = ["male","female","non-binary"]
 
@@ -14,16 +15,16 @@ class Author:
         self.nationality = nationality
         self.gender = gender
 
-valid_book_types = ["Audio","Physical"]
+valid_book_formats = ["Audio", "Physical"]
 valid_book_sources = ["Spotify","Overdrive","Library N","Library M","Gifted","Bought","Borrowed","Public Bookcase N"]
 valid_reading_languages = ["English","German","French"]
 
 class Book:
 
-    def __init__(self,title,author,type,source,owned,date_started,date_finished,series,genre,pub_year,rating,num_times_read,reading_language):
+    def __init__(self,title,author,book_format,source,owned,date_started,date_finished,series,genre,pub_year,rating,num_times_read,reading_language):
 
-        if type not in valid_book_types:
-            raise ValueError(str(type)+" is an invalid book type. Valid options are: "+str(valid_book_types))
+        if book_format not in valid_book_formats:
+            raise ValueError(str(book_format) +" is an invalid book type. Valid options are: " + str(valid_book_formats))
 
         if source not in valid_book_sources:
             raise ValueError(str(source)+" is an invalid book source. Valid options are: "+str(valid_book_sources))
@@ -33,7 +34,7 @@ class Book:
 
         self.title = title
         self.author = author
-        self.type = type
+        self.book_format = book_format
         self.source = source
         self.owned = owned
         self.date_started = date_started
@@ -45,6 +46,18 @@ class Book:
         self.num_times_read = num_times_read
         self.reading_language = reading_language
 
+class Audio_Book(Book):
+    def __init__(self,title,author,book_format,source,owned,date_started,date_finished,series,genre,pub_year,rating,num_times_read,reading_language,audio_length):
+
+        if (type(audio_length) != type(datetime.timedelta())):
+            raise TypeError(str(type(audio_length)) + " is an invalid data type. The valid data type is datetime.timedelta().")
+
+        Book.__init__(self,title,author,book_format,source,owned,date_started,date_finished,series,genre,pub_year,rating,num_times_read,reading_language)
+        self.audio_length = audio_length
+
 charles_duhigg = Author("Charles Duhigg","USA","male")
-the_power_of_habit = Book("The Power of Habit",charles_duhigg,"Audio","Overdrive",False,np.datetime64("2022-02-02"),np.datetime64("2022-02-14"),False,"Selfhelp NF",2012,4.0,1,"English")
+the_power_of_habit = Audio_Book("The Power of Habit", charles_duhigg, "Audio", "Overdrive", False,
+                                np.datetime64("2022-02-02"), np.datetime64("2022-02-14"), False, "Selfhelp NF", 2012,
+                                4.0, 1, "English", datetime.timedelta(hours=10, minutes=53))
 print(the_power_of_habit.author.gender)
+print(the_power_of_habit.audio_length)
