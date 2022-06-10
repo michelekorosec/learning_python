70cf105d33128a8b7e467fadc88e546bc65b6d9d
michelekorosec <72503894+michelekorosec@users.noreply.github.com>
2022-06-10 19:30:41 +0200

    Created book series class

---
+++ Book_DataBase/series_class.py
@@ -0,0 +1,7 @@
+class series():
+
+    def __init__(self,series_title,author,num_books):
+
+        self.series_title = series_title
+        self.author = author
+        self.num_books = num_books
